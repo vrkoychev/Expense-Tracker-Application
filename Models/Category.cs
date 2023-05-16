43c2aed87652af21aa300ffd091d12f64682c1ea
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2023-05-16 09:59:30 +0300

    Initial commit
    Created Category and Transaction classes

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker_Application.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+    }
+}
